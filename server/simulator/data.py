23d4a45295fb9c506a3e554df1d3935dd65397d3
NakagamiYuta <guguru0014@gmail.com>
2019-07-11 17:02:40 +0900

    [add] ゲーム状態を管理するデータクラスを作った

---
+++ server/simulator/data.py
@@ -0,0 +1,17 @@
+class Pos:
+    def __init__(self, team, _id, x, y):
+        self.team = team
+        self._id = _id
+        self.x = x
+        self.y = y
+        self.dx = -10
+        self.dy = -10
+
+
+class Board:
+    def __init__(self, turn, width, height, points, tiled):
+        self.turn = turn
+        self.width = width
+        self.height = height
+        self.points = points
+        self.tiled = tiled
