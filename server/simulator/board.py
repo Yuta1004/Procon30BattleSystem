f9e1d2a6da36a31f10e2d94a81b76eca0f2d82d7
NakagamiYuta <guguru0014@gmail.com>
2019-07-14 16:57:24 +0900

    [fix] 盤面生成処理に誤りがあったため修正

--- server/simulator/board.py
+++ server/simulator/board.py
@@ -1,4 +1,5 @@
 from random import randint
+from copy import deepcopy
 from server.simulator.values import *
 from server.common.functions import gen_2d_list, transpositon_2d_list
 
@@ -16,21 +17,21 @@ def generate_board(width, height, point_upper, point_lower, generate_type=LINE_S
     pass
 
 
-def _generate_line_symmetry_half_A(width, height, point_upper, point_lower):
-    board = gen_2d_list(height, width)
+def _generate_line_symmetry_half_A(width, height, point_lower, point_upper):
+    points = gen_2d_list(height, width)
 
     # 基準となる点配置を作成
-    base_width = (width + 1) / 2
+    base_width = int((width + 1) / 2)
     for y in range(height):
         for x in range(base_width):
-            board[y][x] = randint(point_lower, point_upper)
+            points[y][x] = randint(point_lower, point_upper)
 
     # 対象となるように点を配置していく
-    start_idx = base_width - (1 if len(board) % 2 == 0 else 0)
+    start_idx = base_width - (1 if len(points[0]) % 2 == 1 else 0)
     for y in range(height):
-        board[y][start_idx:] = board[y][:base_width][::-1]
+        points[y][start_idx:] = points[y][:base_width][::-1]
 
-    return board
+    return deepcopy(points)
 
 
 def _generate_line_symmetry_half_B(width, height, point_upper, point_lower):
