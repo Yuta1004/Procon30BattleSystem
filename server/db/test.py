d0f409b157e6abf46e1bb6a1306e7c8a57bc41ec
NakagamiYuta <guguru0014@gmail.com>
2019-07-16 23:49:45 +0900

    [add] BattleDBAccessManagerのテストを追加

--- server/db/test.py
+++ server/db/test.py
@@ -3,10 +3,19 @@ from server.db.battle_db_manager import BattleDBAccessManager
 
 
 def db_manager_test():
-    # dotest("BattleDBAccessManagerTest 1", battle_db_manager_test_1)
-    pass
+    # dotest("BattleDBAccessManagerTest1", battle_db_manager_test_1)
+    dotest("BattleDBAccessManagerTest2", battle_db_manager_test_2)
 
 
 def battle_db_manager_test_1():
     manager = BattleDBAccessManager()
     manager.insert("test_name", "test_token", 10, 30000, 1000, "teams_json")
+
+
+def battle_db_manager_test_2():
+    manager = BattleDBAccessManager()
+    result = manager.get_data(1)
+    keys = ["id", "name", "token", "turn", "turn_msec", "turn_switch_msec", "teams"]
+    values = [1, "test_name", "test_token", 10, 30000, 1000, "teams_json", False]
+    for key, val in zip(keys, values):
+        assert (result[key] == val), "TestFailed"
