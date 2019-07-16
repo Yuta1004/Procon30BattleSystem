6b9b094b66ad6f661a961ed59bcea38bdc2ba6c5
NakagamiYuta <guguru0014@gmail.com>
2019-07-16 22:22:24 +0900

    [add] DBAccessManager作成

---
+++ server/db/db_manager.py
@@ -0,0 +1,28 @@
+import os
+import pymysql.cursors
+
+
+class DBAccessManager:
+
+    def __init__(self):
+        self.conn = pymysql.connect(
+            host="localhost",
+            user="procon30",
+            password=os.environ.get("MYSQL_PASSWORD"),
+            db="procon30",
+            charset="utf8b4",
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+
+    def db_execute(self, deco_func):
+        """
+        DB操作用デコレータ
+        cursorオブジェクトが関数に渡されて、処理終了後は自動でcommitする
+        """
+
+        def db_execute_wrapper():
+            with self.conn.cursor() as cursor:
+                deco_func(cursor)
+            self.conn.commit()
+        return db_execute_wrapper
