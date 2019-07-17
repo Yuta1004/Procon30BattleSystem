d8e6ce45cab41870561898aeafd64f27c40c5e4f
NakagamiYuta <guguru0014@gmail.com>
2019-07-17 15:21:48 +0900

    [add] BattleDBAccessManagerのテストを書いた

--- server/db/test.py
+++ server/db/test.py
@@ -1,10 +1,15 @@
 from server.common.functions import dotest
 from server.db.battle_db_manager import BattleDBAccessManager
+from server.db.action_db_manager import ActionDBAccessManager
 
 
 def db_manager_test():
     # dotest("BattleDBAccessManagerTest1", battle_db_manager_test_1)
     dotest("BattleDBAccessManagerTest2", battle_db_manager_test_2)
+
+#     dotest("ActionDBAccessManagerTest1", action_db_manager_test_1)
+    dotest("ActionDBAccessManagerTest2", action_db_manager_test_2)
+    dotest("ActionDBAccessManagerTest3", action_db_manager_test_3)
 
 
 def battle_db_manager_test_1():
@@ -19,3 +24,27 @@ def battle_db_manager_test_2():
     values = [1, "test_name", "test_token", 10, 30000, 1000, "teams_json", False]
     for key, val in zip(keys, values):
         assert (result[key] == val), "TestFailed"
+
+
+def action_db_manager_test_1():
+    manager = ActionDBAccessManager()
+    manager.insert(1, 1, "test_action")
+
+
+def action_db_manager_test_2():
+    manager = ActionDBAccessManager()
+    result = manager.get_data(1)[0]
+    keys = ["battle_id", "turn", "detail"]
+    values = [1, 1, "test_action"]
+    for key, val in zip(keys, values):
+        assert (result[key] == val), "TestFailed"
+
+
+def action_db_manager_test_3():
+    manager = ActionDBAccessManager()
+    result = manager.get_data(1, 1)
+    keys = ["battle_id", "turn", "detail"]
+    values = [1, 1, "test_action"]
+    for item in result:
+        for key, val in zip(keys, values):
+                assert (item[key] == val), "TestFailed"
