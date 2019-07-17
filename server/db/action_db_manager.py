6e47092fd27fe8dbbe6ee906c7bd72fc7a7c29be
NakagamiYuta <guguru0014@gmail.com>
2019-07-17 19:56:03 +0900

    [add] ActionDBAccessManagerにinsertとget_dataを実装

--- server/db/action_db_manager.py
+++ server/db/action_db_manager.py
@@ -9,3 +9,57 @@ class ActionDBAccessManager(DBAccessManager):
 
     def __del__(self):
         super().__del__()
+
+
+    @DBAccessManager.db_execute
+    def insert(self, cursor, battle_id, turn, detail):
+        """
+        Actionテーブルにデータを追加する
+
+        Params
+        ----------
+        battle_id : int
+            試合ID
+        turn : int
+            ターン
+        detail : str
+            行動情報JSON
+        """
+
+        sql =\
+        """
+            insert into action (battle_id, turn, detail)
+            values(%s, %s, %s)
+        """
+        cursor.execute(sql, (battle_id, turn, detail))
+
+
+    @DBAccessManager.db_execute
+    def get_data(self, cursor, battle_id, turn=None):
+        """
+        Actionテーブルから指定ID、指定ターンのデータを取得する
+
+        Params
+        ----------
+        battle_id : int
+            試合ID
+        turn : int
+            ターン、指定しない場合は検索条件から除外
+
+        Return
+        ----------
+        レコード情報(dict)のリスト
+        """
+
+        sql = "select * from action where battle_id=%s"
+        req_tuple = (battle_id, )
+        if turn is not None:
+            sql += " and turn=%s"
+            req_tuple = (battle_id, turn)
+        cursor.execute(sql, req_tuple)
+        result = cursor.fetchall()
+
+        if len(result) > 0:
+            return result
+        else:
+            return None
