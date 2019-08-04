769764df132f08d87c5db96543cb425e2d9766e4
NakagamiYuta <guguru0014@gmail.com>
2019-08-04 15:13:48 +0900

    [add] /matches実装

--- server/views/match_api.py
+++ server/views/match_api.py
@@ -1,12 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from server import base_route
+from server.api_func.matches import get_all_matches
 
 route_match = Blueprint(__name__, "match-api")
 
 
-@route_match.route(base_route + "/matches")
+@route_match.route(base_route + "/matches", methods=["GET"])
 def matches_top():
-    return "Return Match List(GET)"
+    token = request.headers.get("Authorization")
+    match_list = get_all_matches(token)
+    return jsonify(match_list)
 
 
 @route_match.route(base_route + "/matches/<battle_id>")
