3654b00ccfd297a6dc6fdc9a2c988c5f44046c1b
NakagamiYuta <guguru0014@gmail.com>
2019-08-04 20:59:49 +0900

    [add] データ全消去、テーブル作成の操作を書いたSQLファイルを雑に書いた

---
+++ sql/create.sql
@@ -0,0 +1,4 @@
+create table team (id int auto_increment, name text, token text, primary key(id));
+create table battle (id int auto_increment primary key, name text, start_at_unix_time int, turn int, turn_mills int, interval_mills int, teamA int, teamB int, now_battle int, foreign key(teamA) references team(id), foreign key(teamB) references team(id));
+create table action (battle_id int, turn int, detail text, primary key(battle_id, turn), foreign key (battle_id) references battle(id));
+create table stage (battle_id int, width int, height int, points text, tiled text, agent_pos text, foreign key (battle_id) references battle(id));
