6adc3da5a569797f407d3675f63edbc6d65cd8ef
NakagamiYuta <guguru0014@gmail.com>
2019-08-04 19:43:24 +0900

    [change] /pingの仕様変更

--- server/views/ping_api.py
+++ server/views/ping_api.py
@@ -1,9 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from server import base_route
+from server.api_func.check import token_check
 
 route_ping = Blueprint(__name__, "ping")
 
 
 @route_ping.route(base_route + "/ping")
 def ping():
-    return jsonify(status="OK")
+    token = request.headers.get("Authorization")
+    is_error, status, response = token_check(token)
+    if is_error:
+        return jsonify(response), status
+    else:
+        return jsonify(status="OK"), 200
