beaa1db1ccbfe673faceb034e6bab7679f8a2454
NakagamiYuta <guguru0014@gmail.com>
2019-08-05 16:57:06 +0900

    [change] APIからデータを持ってきて出力するまでの処理を独立させた

--- cui_client/table_stdout.py
+++ cui_client/table_stdout.py
@@ -1,4 +1,19 @@
+import urllib3
+import json;
 from tabulate import tabulate
+
+
+def get_data_and_output(api_url):
+    http_connecter = urllib3.PoolManager()
+    result = http_connecter.request("GET", api_url)
+
+    if result.status == 200:
+        result_json = json.loads(result.data.decode())
+        if type(result_json) != list:
+            result_json = [result_json]
+        table_output(result_json)
+    else:
+        print("no data")
 
 
 def table_output(table_items_json):
