70f470d078ca32fbba1f55e18eb041bf9bbb18ec
NakagamiYuta <guguru0014@gmail.com>
2019-08-05 13:51:44 +0900

    [add] CUIClientに set host コマンドを実装

--- cui_client/cui_client.py
+++ cui_client/cui_client.py
@@ -5,6 +5,8 @@ host_url = "http://localhost:16000/procon30-battle-api"
 
 
 def exec_command(command):
+    global host_url
+
     # Check
     if command[0] == "check":
         if len(command) == 1:
@@ -16,6 +18,25 @@ def exec_command(command):
             return
 
         print("Usage : check [connection]")
+        return
+
+    # Set
+    if command[0] == "set":
+        if len(command) == 1:
+            print("Usage : set [host]")
+            return
+
+        if command[1] == "host":
+            print("input new host : ", end="")
+            new_host = input()
+            print("update ok? (", new_host, ") y/N : ", end="")
+            res = input()
+            if res == "y" or res == "Y":
+                host_url = new_host
+                print("update successed!")
+            return
+
+        print("Usage : set [host]")
         return
 
 
