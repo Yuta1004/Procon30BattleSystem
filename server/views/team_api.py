612e969d1ad23a0a8cba1d5b474fa94b6f653452
NakagamiYuta <guguru0014@gmail.com>
2019-08-05 16:34:37 +0900

    [add] /team/id/{team_id}, /team/token/{token} を実装

--- server/views/team_api.py
+++ server/views/team_api.py
@@ -10,3 +10,23 @@ def team_top():
     team_db_manager = TeamDBAccessManager()
     team_list = team_db_manager.get_data()
     return jsonify(team_list), 200
+
+
+@route_team.route(base_route + "/team/id/<team_id>")
+def team_view_id(team_id):
+    team_db_manager = TeamDBAccessManager()
+    team = team_db_manager.get_data(team_id=team_id)
+    if len(team) == 0:
+        return jsonify(status="InvalidTeamID"), 400
+
+    return jsonify(team[0]), 200
+
+
+@route_team.route(base_route + "/team/token/<token>")
+def team_view_token(token):
+    team_db_manager = TeamDBAccessManager()
+    team = team_db_manager.get_data(token=token)
+    if len(team) == 0:
+        return jsonify(status="InvalidToken"), 400
+
+    return jsonify(team[0]), 200
