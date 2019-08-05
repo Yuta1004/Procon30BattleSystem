f15ab886d33cd45cf783c8ceff13890783223c83
NakagamiYuta <guguru0014@gmail.com>
2019-08-05 16:59:06 +0900

    [change] show_battle系の関数の処理を変更

--- cui_client/show_battle.py
+++ cui_client/show_battle.py
@@ -1,27 +1,13 @@
 import urllib3
 import json;
-from cui_client.table_stdout import table_output
+from cui_client.table_stdout import get_data_and_output, table_output
 
 
 def show_battle(host_url):
     api_url = host_url + "/battle"
-    http_connecter = urllib3.PoolManager()
-    result = http_connecter.request("GET", api_url)
-
-    if result.status == 200:
-        result_json = json.loads(result.data.decode())
-        table_output(result_json)
-    else:
-        print("no data")
+    get_data_and_output(api_url)
 
 
 def show_battle_id(host_url, battle_id):
     api_url = host_url + "/battle/" + str(battle_id)
-    http_connecter = urllib3.PoolManager()
-    result = http_connecter.request("GET", api_url)
-
-    if result.status == 200:
-        result_json = json.loads(result.data.decode())
-        table_output([result_json])
-    else:
-        print("no data")
+    get_data_and_output(api_url)
